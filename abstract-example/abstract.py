3a12ee11db8fb4254d5cc000e5b72d4b568c5cbe
Yuan Hang Gan <yuanhanggan@seas.harvard.edu>
2026-09-03 19:58:05 -0400

    some more settings

---
+++ abstract-example/abstract.py
@@ -0,0 +1,5 @@
+# Note that for each example in any of these pyomo things, we need to follow these steps 
+# Create model -> instantiate the model -> apply the solver -> interrogate the solver results 
+
+import pyomo.environ as pyo
+ 
