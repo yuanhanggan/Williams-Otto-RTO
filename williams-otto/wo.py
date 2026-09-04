639c3616134c1d084fa95593d5365ec31eb381cc
Yuan Hang Gan <yuanhanggan@seas.harvard.edu>
2026-09-04 09:45:57 -0400

    finsihed with the first equality constarint

--- williams-otto/wo.py
+++ williams-otto/wo.py
@@ -16,5 +16,21 @@ model.W = pyo.Param(within=pyo.NonNegativeReals) # 1x1
 model.x = pyo.Param(model.I, within=pyo.UnitInterval) # nx1
 model.fa = pyo.Param(within=pyo.NonNegativeReals) # 1x1 
 
-model.fb = pyo.Var(initialize=5)
+model.fb = pyo.Var(initialize=6, within=pyo.NonNegativeReals) # 1x1 
+model.Tr = pyo.Var(initialize=100, within=pyo.NonNegativeReals) # 1x1
 
+# Rate equation
+def k(amodel, I):
+    return amodel.A[I] * pyo.exp(amodel.Ea[I] / amodel.Tr)
+
+# Objective expression 
+def obj_rule(amodel):
+    return 5554.1 * (amodel.fa + amodel.fb) * amodel.x[6] \
+    + 125.91 * (amodel.fa + amodel.fb) * amodel.x[4] \
+    - 370.3 * amodel.fa \
+    - 555.42 * amodel.fb
+
+# Equality constraint expressions
+def xa_bal(amodel):
+    return amodel.fa - (amodel.fa + amodel.fb) * amodel.x[1] \
+    - k(amodel, 1) * amodel.x[1] * amodel.x[2] * amodel.W
