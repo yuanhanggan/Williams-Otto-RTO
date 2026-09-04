eedc850f7aa9961ea85e3f98602226edefa3b97d
Yuan Hang Gan <yuanhanggan@seas.harvard.edu>
2026-09-04 13:04:34 -0400

    after lunch

--- williams-otto/wo.py
+++ williams-otto/wo.py
@@ -16,7 +16,7 @@ model.fa = pyo.Param(within=pyo.PositiveReals) # 1x1
 model.fb_cons = pyo.Param(pyo.RangeSet(1, 2), within=pyo.PositiveReals) # 2x1 
 model.Tr_cons = pyo.Param(pyo.RangeSet(1, 2), within=pyo.PositiveReals) # 2x1
 
-model.x = pyo.Var(model.J, within=pyo.PositiveReals) # mx1
+model.x = pyo.Var(model.J, within=pyo.PositiveReals) # nx1
 model.fb = pyo.Var(within=pyo.PositiveReals) # 1x1 
 model.Tr = pyo.Var(within=pyo.PositiveReals) # 1x1
 
@@ -26,32 +26,32 @@ def k(am, I):
 
 # Objective expression 
 def obj_rule(am):
-    return -1 * (5554.1 * (am.fa + am.fb) * am.x[6] \
-    + 125.91 * (am.fa + am.fb) * am.x[4] \
-    - 370.3 * am.fa \
-    - 555.42 * am.fb)
+    return -1 * ((5554.1 * (am.fa + am.fb) * am.x[6]) \
+    + (125.91 * (am.fa + am.fb) * am.x[4]) \
+    - (370.3 * am.fa) \
+    - (555.42 * am.fb))
 
 # Equality constraint expressions
 def xa_bal(am):
-    return 0 == am.fa - (am.fa + am.fb) * am.x[1] \
-    - k(am, 1) * am.x[1] * am.x[2] * am.W
+    return 0 == am.fa - ((am.fa + am.fb) * am.x[1]) \
+    - (k(am, 1) * am.x[1] * am.x[2] * am.W)
 def xb_bal(am):
-    return 0 == am.fb - (am.fa + am.fb) * am.x[2] \
+    return 0 == am.fb - ((am.fa + am.fb) * am.x[2]) \
     - (k(am, 1) * am.x[1] * am.x[2] * am.W) \
     - (k(am, 2) * am.x[2] * am.x[3] * am.W)
 def xc_bal(am):
-    return 0 == -1 * (am.fa + am.fb) * am.x[3] \
+    return 0 == (-1 * (am.fa + am.fb) * am.x[3]) \
     + 2 * (k(am, 1) * am.x[1] * am.x[2] * am.W) \
     - 2 * (k(am, 2) * am.x[2] * am.x[3] * am.W) \
-    - k(am, 3) * am.x[3] * am.x[6] * am.W
+    - (k(am, 3) * am.x[3] * am.x[6] * am.W)
 def xe_bal(am):
-    return 0 == -1 * (am.fa + am.fb) * am.x[4] \
-    + (k(am, 2) * am.x[2] * am.x[3] * am.W)
+    return 0 == (-1 * (am.fa + am.fb) * am.x[4]) \
+    + 2 * (k(am, 2) * am.x[2] * am.x[3] * am.W)
 def xg_bal(am):
-    return 0 == -1 * (am.fa + am.fb) * am.x[5] \
+    return 0 == (-1 * (am.fa + am.fb) * am.x[5]) \
     + 1.5 * (k(am, 3) * am.x[3] * am.x[6] * am.W)
 def xp_bal(am):
-    return 0 == -1 * (am.fa + am.fb) * am.x[6] \
+    return 0 == (-1 * (am.fa + am.fb) * am.x[6]) \
     + (k(am, 2) * am.x[2] * am.x[3] * am.W) \
     - 0.5 * (k(am, 3) * am.x[3] * am.x[6] * am.W)
 
@@ -63,7 +63,7 @@ def tr_rule(am):
     return pyo.inequality(am.Tr_cons[1], am.Tr, am.Tr_cons[2])
 
 # Instantiate 
-model.obj = pyo.Objective(rule=obj_rule)
+model.obj = pyo.Objective(rule=obj_rule, sense=pyo.minimize)
 model.xa_bal = pyo.Constraint(rule=xa_bal)
 model.xb_bal = pyo.Constraint(rule=xb_bal)
 model.xc_bal = pyo.Constraint(rule=xc_bal)
