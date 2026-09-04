381489a02490ae79b3a32500cbbf341d3620a4df
Yuan Hang Gan <yuanhanggan@seas.harvard.edu>
2026-09-04 09:55:46 -0400

    i think that is all of the equality constraints

--- williams-otto/wo.py
+++ williams-otto/wo.py
@@ -20,17 +20,41 @@ model.fb = pyo.Var(initialize=6, within=pyo.NonNegativeReals) # 1x1
 model.Tr = pyo.Var(initialize=100, within=pyo.NonNegativeReals) # 1x1
 
 # Rate equation
-def k(amodel, I):
-    return amodel.A[I] * pyo.exp(amodel.Ea[I] / amodel.Tr)
+def k(am, I):
+    return am.A[I] * pyo.exp(am.Ea[I] / am.Tr)
 
 # Objective expression 
-def obj_rule(amodel):
-    return 5554.1 * (amodel.fa + amodel.fb) * amodel.x[6] \
-    + 125.91 * (amodel.fa + amodel.fb) * amodel.x[4] \
-    - 370.3 * amodel.fa \
-    - 555.42 * amodel.fb
+def obj_rule(am):
+    return 5554.1 * (am.fa + am.fb) * am.x[6] \
+    + 125.91 * (am.fa + am.fb) * am.x[4] \
+    - 370.3 * am.fa \
+    - 555.42 * am.fb
 
 # Equality constraint expressions
-def xa_bal(amodel):
-    return amodel.fa - (amodel.fa + amodel.fb) * amodel.x[1] \
-    - k(amodel, 1) * amodel.x[1] * amodel.x[2] * amodel.W
+def xa_bal(am):
+    return 0 == am.fa - (am.fa + am.fb) * am.x[1] \
+    - k(am, 1) * am.x[1] * am.x[2] * am.W
+
+def xb_bal(am):
+    return 0 == am.fb - (am.fa + am.fb) * am.x[2] \
+    - (k(am, 1) * am.x[1] * am.x[2] * am.W) \
+    - (k(am, 2) * am[2] * am[3] * am.W)
+
+def xc_bal(am):
+    return 0 == -1 * (am.fa + am.fb) * am.x[3] \
+    + 2 * (k(am, 1) * am.x[1] * am.x[2] * am.W) \
+    - 2 * (k(am, 2) * am[2] * am[3] * am.W) \
+    - k(am, 3) * am[3] * am[6] * am.W
+
+def xe_bal(am):
+    return 0 == -1 * (am.fa + am.fb) * am.x[4] \
+    + (k(am, 2) * am[2] * am[3] * am.W)
+
+def xg_bal(am):
+    return 0 == -1 * (am.fa + am.fb) * am.x[5] \
+    + 1.5 * (k(am, 3) * am[3] * am[6] * am.W)
+
+def xp_bal(am):
+    return 0 == -1 * (am.fa + am.fb) * am.x[6] \
+    + (k(am, 2) * am[2] * am[3] * am.W) \
+    - 0.5 * (k(am, 3) * am[3] * am[6] * am.W)
