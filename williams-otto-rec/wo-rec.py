b34dd9c0a791298dc01236d6c9ab7f3d5415cc4d
Yuan Hang Gan <yuanhanggan@seas.harvard.edu>
2026-09-21 17:39:35 -0400

    the recursive algo works but we have drift

--- williams-otto-rec/wo-rec.py
+++ williams-otto-rec/wo-rec.py
@@ -9,7 +9,7 @@ da.load(filename='wo-rec.dat')
 sv_dir = os.path.join(os.getcwd(), 'sims', 'recursion')
 da_r = pd.read_csv(os.path.join(sv_dir, 'wo.csv')).set_index('t').dropna().to_dict(orient='index')
 t_is = list(da_r.keys())
-x_is = [1, 2, 3, 4, 5, 6]
+x_is = ['x_1', 'x_2', 'x_3', 'x_4', 'x_5', 'x_6']
 
 # Parameters 
 mo.dt = Param(initialize=da['dt']) # 1x1 
@@ -41,42 +41,42 @@ def k(am, I, t):
 def _xa_rule(am, t):
     if t == am.t.first(): 
         return Constraint.Skip
-    return am.dx_dt[1, t] == (1 / am.W) * (am.fa[t] - ((am.fa[t] + am.fb[t]) * am.x[1, t]) \
-    - (k(am, 1, t) * am.x[1, t] * am.x[2, t] * am.W))
+    return am.dx_dt['x_1', t] == (1 / am.W) * (am.fa[t] - ((am.fa[t] + am.fb[t]) * am.x['x_1', t]) \
+    - (k(am, 1, t) * am.x['x_1', t] * am.x['x_2', t] * am.W))
 mo.xa_rule = Constraint(mo.t, rule=_xa_rule)
 def _xb_rule(am, t):
     if t == am.t.first(): 
         return Constraint.Skip
-    return am.dx_dt[2, t] == (1 / am.W) * (am.fb[t] - ((am.fa[t] + am.fb[t]) * am.x[2, t]) \
-    - (k(am, 1, t) * am.x[1, t] * am.x[2, t] * am.W) \
-    - (k(am, 2, t) * am.x[2, t] * am.x[3, t] * am.W))
+    return am.dx_dt['x_2', t] == (1 / am.W) * (am.fb[t] - ((am.fa[t] + am.fb[t]) * am.x['x_2', t]) \
+    - (k(am, 1, t) * am.x['x_1', t] * am.x['x_2', t] * am.W) \
+    - (k(am, 2, t) * am.x['x_2', t] * am.x['x_3', t] * am.W))
 mo.xb_rule = Constraint(mo.t, rule=_xb_rule)
 def _xc_rule(am, t): 
     if t == am.t.first(): 
         return Constraint.Skip
-    return am.dx_dt[3, t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x[3, t]) \
-    + 2 * (k(am, 1, t) * am.x[1, t] * am.x[2, t] * am.W) \
-    - 2 * (k(am, 2, t) * am.x[2, t] * am.x[3, t] * am.W) \
-    - (k(am, 3, t) * am.x[3, t] * am.x[6, t] * am.W))
+    return am.dx_dt['x_3', t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x['x_3', t]) \
+    + 2 * (k(am, 1, t) * am.x['x_1', t] * am.x['x_2', t] * am.W) \
+    - 2 * (k(am, 2, t) * am.x['x_2', t] * am.x['x_3', t] * am.W) \
+    - (k(am, 3, t) * am.x['x_3', t] * am.x['x_6', t] * am.W))
 mo.xc_rule = Constraint(mo.t, rule=_xc_rule)
 def _xe_rule(am, t): 
     if t == am.t.first(): 
         return Constraint.Skip
-    return am.dx_dt[4, t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x[4, t]) \
-    + 2 * (k(am, 2, t) * am.x[2, t] * am.x[3, t] * am.W))
+    return am.dx_dt['x_4', t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x['x_4', t]) \
+    + 2 * (k(am, 2, t) * am.x['x_2', t] * am.x['x_3', t] * am.W))
 mo.xe_rule = Constraint(mo.t, rule=_xe_rule)
 def _xg_rule(am, t): 
     if t == am.t.first(): 
         return Constraint.Skip
-    return am.dx_dt[5, t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x[5, t]) \
-    + 1.5 * (k(am, 3, t) * am.x[3, t] * am.x[6, t] * am.W))
+    return am.dx_dt['x_5', t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x['x_5', t]) \
+    + 1.5 * (k(am, 3, t) * am.x['x_3', t] * am.x['x_6', t] * am.W))
 mo.xg_rule = Constraint(mo.t, rule=_xg_rule)
 def _xp_rule(am, t): 
     if t == am.t.first(): 
         return Constraint.Skip
-    return am.dx_dt[6, t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x[6, t]) \
-    + (k(am, 2, t) * am.x[2, t] * am.x[3, t] * am.W) \
-    - 0.5 * (k(am, 3, t) * am.x[3, t] * am.x[6, t] * am.W))
+    return am.dx_dt['x_6', t] == (1 / am.W) * ((-1 * (am.fa[t] + am.fb[t]) * am.x['x_6', t]) \
+    + (k(am, 2, t) * am.x['x_2', t] * am.x['x_3', t] * am.W) \
+    - 0.5 * (k(am, 3, t) * am.x['x_3', t] * am.x['x_6', t] * am.W))
 mo.xp_rule = Constraint(mo.t, rule=_xp_rule)
 
 # Boundary conditions 
@@ -97,7 +97,7 @@ solver.options['halt_on_ampl_error'] = 'yes'
 results = solver.solve(mo, tee=True, keepfiles=True, logfile = os.path.join(sv_dir, 'wo.log'))
 
 # Saving results 
-res = pd.DataFrame()
+res = pd.DataFrame(index=pd.Index([value(mo.t.last())], name='t'))
 old = pd.read_csv(os.path.join(sv_dir, 'wo.csv')).set_index('t').dropna()
 bounds = {}
 res.index.name = 't'
@@ -108,9 +108,9 @@ for v in mo.component_objects(Var, active = True):
         bounds[v.name] = (v[mo.t.first()].lb, v[mo.t.first()].ub)
     elif v.dim() == 2: 
         J, _ = subsets
-        for j in J:   
-            res[f'{v.name}_{j}'] = [value(v[j, mo.t.last()])]
-            bounds[f'{v.name}_{j}'] = (v[j, mo.t.first()].lb, v[j, mo.t.first()].ub) 
+        for j_idx, j in enumerate(J, start=1):   
+            res[f'{v.name}_{j_idx}'] = [value(v[j, mo.t.last()])]
+            bounds[f'{v.name}_{j_idx}'] = (v[j, mo.t.first()].lb, v[j, mo.t.first()].ub) 
 # res.to_csv(os.path.join(sv_dir, 'wo.csv'), mode='a', header=False, index=True)
 res = pd.concat([old, res]).to_csv(os.path.join(sv_dir, 'wo.csv'), index=True)
 with open(os.path.join(sv_dir, 'wo.txt') , 'w') as file:
